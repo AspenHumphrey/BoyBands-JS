8e526713799392c331e30eb426cd87a6c176adee
Aspen Humphrey <aspenjadedimmick@gmail.com>
2017-06-08 09:32:54 -0500

    loops and unctions

--- boybands.js
+++ boybands.js
@@ -2,28 +2,32 @@ var bands = ["Boyz II Men", "NSync", "New Kids on the Block", "98 Degrees", "One
 var vegetables = ["Carrots", "Kale", "Zucchini", "Broccoli", "Squash"];
 
 // The number of loops to perform (what if the array changes?)
-var loopCount = 5;
+var loopCount = [bands, vegetables]
+for (loopCount = 0; loopCount = loopCount.length) {
+
+};
+console.log(loopCount[i]);
 
 // Keep track of which band we're on in the loop
-var currentBand = "";
+var currentBand = bands[i];
 
 // Keep track of which veggie we're on in the loop
-var currentVeggie = "";
+var currentVeggie = vegetables[i];
 
 // Get a reference to the appropriate DOM element for bands
-var bandElement = document.getElementById(???);
+var bandElement = document.getElementById("boy-bands");
 
 // Get a reference to the appropriate DOM element for vegetables
-var veggieElement = ???;
+var veggieElement = document.getElementById("vegetables");
 
 // Start looping
 for (var loopTracker = 0; loopTracker < loopCount; loopTracker += 1) {
   
   // Add the band names into the correct <div>
-  currentBand = ???;
+  currentBand = "";
 
   
   // Add the veggie names into the correct <div>
-  currentVeggie = ???;
+  currentVeggie = "";
 
 }
