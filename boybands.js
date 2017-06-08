29c24ccb6bf26cdf43a4e1d266646c6a4058d88c
Aspen Humphrey <aspenjadedimmick@gmail.com>
2017-06-08 11:00:59 -0500

    with bakers questioning i was able to talk myself through the process and fit the parts together. ive noticed that I understand the concept but it is hard for me to articlate what I am doing without talking it out and writing it down during.

--- boybands.js
+++ boybands.js
@@ -2,32 +2,59 @@ var bands = ["Boyz II Men", "NSync", "New Kids on the Block", "98 Degrees", "One
 var vegetables = ["Carrots", "Kale", "Zucchini", "Broccoli", "Squash"];
 
 // The number of loops to perform (what if the array changes?)
-var loopCount = [bands, vegetables]
-for (loopCount = 0; loopCount = loopCount.length) {
 
-};
-console.log(loopCount[i]);
+	var loopCount = bands.length;
 
-// Keep track of which band we're on in the loop
+for (var i = 0; i < loopCount; i++) {
+	console.log(`${bands[i]} ${vegetables[i]}`);
+
 var currentBand = bands[i];
-
-// Keep track of which veggie we're on in the loop
 var currentVeggie = vegetables[i];
 
-// Get a reference to the appropriate DOM element for bands
-var bandElement = document.getElementById("boy-bands");
+	poop(currentBand, currentVeggie);
+}
 
-// Get a reference to the appropriate DOM element for vegetables
-var veggieElement = document.getElementById("vegetables");
+function poop(currentBand, currentVeggie) {
+	var bandElement = document.getElementById("boy-bands");
+	var newNewBandElement = document.createElement ("p");
+	var newBandElement = document.createTextNode(currentBand);
 
-// Start looping
-for (var loopTracker = 0; loopTracker < loopCount; loopTracker += 1) {
-  
-  // Add the band names into the correct <div>
-  currentBand = "";
+	newNewBandElement.appendChild(newBandElement);
 
-  
-  // Add the veggie names into the correct <div>
-  currentVeggie = "";
+	var veggieElement = document.getElementById("vegetables");
+	var newNewVeggieElement = document.createElement ("p");
+	var newVeggieElement = document.createTextNode(currentVeggie);
+
+	newNewVeggieElement.appendChild(newVeggieElement);
+
+	bandElement.appendChild(newNewBandElement);
+
+	veggieElement.appendChild(newNewVeggieElement);
 
 }
+
+
+
+ // Keep track of which band we're on in the loop
+ //var currentBand = bands[i];
+
+// Keep track of which veggie we're on in the loop
+//var currentVeggie = vegetables[i];
+
+// // Get a reference to the appropriate DOM element for bands
+// var bandElement = document.getElementById("boy-bands");
+
+// // Get a reference to the appropriate DOM element for vegetables
+// var veggieElement = document.getElementById("vegetables");
+
+// // Start looping
+// for (var loopTracker = 0; loopTracker < loopCount; loopTracker += 1) {
+  
+//   // Add the band names into the correct <div>
+//   currentBand = "";
+
+  
+//   // Add the veggie names into the correct <div>
+//   currentVeggie = "";
+
+// }
